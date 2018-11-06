1bf8d8612401e3a2212d52b975b92b132dc36ba7
Kevin Toh <kevintoh0305@gmail.com>
2018-11-06 17:11:47 +0800

    Completed the UI structure for the Login Page, Account Registration Page, Hello Page and Gender Selection Page. Requires image vectors and logos to complete the UI design.

--- app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
@@ -1,13 +1,27 @@
 package com.kevintoh0305gmail.gastronome;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        button = findViewById(R.id.btnLogin);
+
     }
+
+    public void onClick(View v) {
+        startActivity(new Intent(this, HelloPage.class));
+    }
+
+    public void onRegistrationClick(View v) {
+        startActivity(new Intent(this, Registration.class));
+    }
+
 }
