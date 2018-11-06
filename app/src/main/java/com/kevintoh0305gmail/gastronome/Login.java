7236da8bfb2cc33db91b622526ae464d91699f2f
Kevin Toh <kevintoh0305@gmail.com>
2018-11-06 12:59:12 +0800

    Completed login UI structure, elements and alignment. Need the logo and background to fully completed the login page.

--- app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
@@ -2,12 +2,12 @@ package com.kevintoh0305gmail.gastronome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class Login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
