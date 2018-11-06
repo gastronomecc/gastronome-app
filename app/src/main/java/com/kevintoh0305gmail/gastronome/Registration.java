2d24e833ec9b40f8f574d944774a1c1ff547fb79
Dominic Ng Jun Yao <s10177858@connect.np.edu.sg>
2018-11-06 20:27:10 +0800

    Connect to Firebase Auth and Realtime Database

--- app/src/main/java/com/kevintoh0305gmail/gastronome/Registration.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Registration.java
@@ -1,10 +1,10 @@
 package com.kevintoh0305gmail.gastronome;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Registration extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
