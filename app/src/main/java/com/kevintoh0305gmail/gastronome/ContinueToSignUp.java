a5e5dd0b3d097320a7a7d386e52a71ac6adfbd75
Kevin Toh <kevintoh0305@gmail.com>
2018-11-07 20:44:28 +0800

    Complete UIs for registration and added a placeholder for the homepage

---
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/ContinueToSignUp.java
@@ -0,0 +1,13 @@
+package com.kevintoh0305gmail.gastronome;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContinueToSignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_continue_to_sign_up);
+    }
+}
--- app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
