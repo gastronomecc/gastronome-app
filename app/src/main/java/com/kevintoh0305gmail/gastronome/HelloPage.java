e12a3ca09e58a324d47ee64fa01151c410d3d243
Kevin Toh <kevintoh0305@gmail.com>
2018-11-14 02:28:01 +0800

    Remove sign up textview on login page.

--- app/src/main/java/com/kevintoh0305gmail/gastronome/HelloPage.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/HelloPage.java
@@ -5,28 +5,21 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class HelloPage extends AppCompatActivity {
     static RegisteringAccountProfile profile;
-    FloatingActionButton fab;
-    TextView txtLogin;
+    Button btnNext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_page);
-        fab = findViewById(R.id.fabnextpage1);
-        txtLogin = findViewById(R.id.tvHelloPageText4);
-        fab.setOnClickListener(new View.OnClickListener() {
+        btnNext = findViewById(R.id.btnNext);
+        btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(HelloPage.this, GenderSelection.class));
-            }
-        });
-        txtLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(HelloPage.this, Login.class));
+                startActivity(new Intent(HelloPage.this, InputAge.class));
             }
         });
     }
