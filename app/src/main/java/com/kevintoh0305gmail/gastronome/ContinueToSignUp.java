b8843ad5bda99596846e0777530097c05bc294e4
Kevin Toh <kevintoh0305@gmail.com>
2018-11-13 20:13:32 +0800

    Add scrollview to recipe info and fix missing link to login when the user continues to sign up.

--- app/src/main/java/com/kevintoh0305gmail/gastronome/ContinueToSignUp.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/ContinueToSignUp.java
@@ -5,18 +5,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class ContinueToSignUp extends AppCompatActivity {
     Button btnEmailSignUp;
+    TextView login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_continue_to_sign_up);
         btnEmailSignUp = findViewById(R.id.btnContinueEmail);
+        login = findViewById(R.id.tvContinueText2);
         btnEmailSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(ContinueToSignUp.this, Registration.class));
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ContinueToSignUp.this, Login.class));
             }
         });
     }
