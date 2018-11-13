42bd209cac0514349bf3bb9dadaed99b178c1135
Dominic Ng Jun Yao <s10177858@connect.np.edu.sg>
2018-11-13 09:32:44 +0800

    Update Recipe Class

--- app/src/main/java/com/kevintoh0305gmail/gastronome/Recipe.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Recipe.java
@@ -11,6 +11,15 @@ public class Recipe {
     List<String> instructions;
     String servSize;
     int calories;
+    String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 
     public String getTitle() {
         return title;
