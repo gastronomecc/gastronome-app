764bc13834c61764a1104be3b2aa39c44e500454
Dominic Ng Jun Yao <s10177858@connect.np.edu.sg>
2018-11-13 16:27:30 +0800

    Complete displaying of all Recipes

--- app/src/main/java/com/kevintoh0305gmail/gastronome/RecipeViewHolder.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/RecipeViewHolder.java
@@ -5,6 +5,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,17 +15,21 @@ public class RecipeViewHolder extends RecyclerView.ViewHolder{
     public TextView txtTitle;
     public TextView txtShortDesc;
     public View view;
+    public Button btnPrepTime;
+    public Button btnDifficulty;
+    public Button btnMealType;
     private Context context;
 
     public RecipeViewHolder(View v, Context c)
     {
         super(v);
         view = v;
-        wv = v.findViewById(R.id.wvPic);
+        //wv = v.findViewById(R.id.wvPic);
         txtTitle = v.findViewById(R.id.txtTitle);
         txtShortDesc = v.findViewById(R.id.txtShortDesc);
+        btnPrepTime = v.findViewById(R.id.btnPrepTimeTag);
+        btnMealType = v.findViewById(R.id.btnMealTypeTag);
+        btnDifficulty = v.findViewById(R.id.btnDifficultyTag);
         context = c;
     }
-
-
 }
