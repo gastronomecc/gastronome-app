fe99b4f1b790e3b9c7a0c70cbdae6d15ba330c65
Dominic Ng Jun Yao <s10177858@connect.np.edu.sg>
2018-11-14 10:50:18 +0800

    Display recipes in weekly log

--- app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
@@ -32,7 +32,7 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in = new Intent(Login.this, RecipeActivity.class);
+                Intent in = new Intent(Login.this, WeekyLog.class);
                 startActivity(in);
                 String email = txtEmail.getText().toString().trim();
                 String password = txtPassword.getText().toString().trim();
