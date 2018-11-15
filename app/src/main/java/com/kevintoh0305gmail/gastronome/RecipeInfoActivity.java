81bb6ab022f0f22c1a30b37ec15f3a549d5db04a
Dominic Ng Jun Yao <s10177858@connect.np.edu.sg>
2018-11-15 12:09:13 +0800

    Add new Logs into Firebase when created

--- app/src/main/java/com/kevintoh0305gmail/gastronome/RecipeInfoActivity.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/RecipeInfoActivity.java
@@ -15,7 +15,11 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 public class RecipeInfoActivity extends AppCompatActivity {
 
@@ -24,6 +28,11 @@ public class RecipeInfoActivity extends AppCompatActivity {
     ImageButton imgBtnAdd;
     RecyclerView rvIngredients;
     IngredientsAdapter ingredientsAdapter;
+    Button btnSun, btnMon, btnTues, btnWed, btnThurs, btnFri, btnSat;
+    Boolean sunSelect, monSelect, tuesSelect, wedSelect, thursSelect, friSelect, satSelect;
+    ArrayList<String> days = new ArrayList<>();
+    FirebaseDatabase database;
+    String title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,9 +52,12 @@ public class RecipeInfoActivity extends AppCompatActivity {
         btnIncrease = findViewById(R.id.btnRecipeInfoAddServing);
         btnDecrease = findViewById(R.id.btnRecipeInfoRemoveServing);
         imgBtnAdd = findViewById(R.id.imgBtnAdd);
+        database = FirebaseDatabase.getInstance();
+
+
 
         Recipe selectedRecipe = RecipeAdapter.selectedRecipe;
-        String title = selectedRecipe.getTitle();
+        title = selectedRecipe.getTitle();
         String shortDesc = selectedRecipe.getShortDesc();
         String prepTime = "" + selectedRecipe.getPrepTime();
         String difficulty = selectedRecipe.getDifficulty();
@@ -57,7 +69,7 @@ public class RecipeInfoActivity extends AppCompatActivity {
         String carbs = selectedRecipe.getCarbs();
         String protein = selectedRecipe.getProtein();
         String sugar = selectedRecipe.getSugar();
-        String salt = selectedRecipe.getSalt();
+        final String salt = selectedRecipe.getSalt();
         final ArrayList<String> nutrients = new ArrayList<>();
         nutrients.add("Calories = " + selectedRecipe.getCalories());
         nutrients.add("Fats = " + fats);
@@ -72,8 +84,7 @@ public class RecipeInfoActivity extends AppCompatActivity {
         btnDifficulty.setText(difficulty);
         if (dietary.equals("None")) {
             btnDietary.setVisibility(View.INVISIBLE);
-        }
-        else {
+        } else {
             btnDietary.setText(dietary);
         }
 
@@ -82,10 +93,16 @@ public class RecipeInfoActivity extends AppCompatActivity {
         imgBtnAdd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                sunSelect = false;
+                monSelect = false;
+                tuesSelect = false;
+                wedSelect = false;
+                thursSelect = false;
+                friSelect = false;
+                satSelect = false;
                 imgBtnAddClick();
             }
         });
-
 
 
         btnIngredients.setOnClickListener(new View.OnClickListener() {
@@ -136,7 +153,7 @@ public class RecipeInfoActivity extends AppCompatActivity {
         btnIncrease.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String serving = "" +  tvServSize.getText();
+                String serving = "" + tvServSize.getText();
                 int newServe = Integer.parseInt(serving);
                 newServe++;
                 tvServSize.setText("" + newServe);
@@ -164,12 +181,112 @@ public class RecipeInfoActivity extends AppCompatActivity {
     public void imgBtnAddClick() {
         LayoutInflater layoutInflater = LayoutInflater.from(getApplicationContext());
         final View view = layoutInflater.inflate(R.layout.logdialog, null);
+        btnMon = view.findViewById(R.id.btnMon);
+        btnTues = view.findViewById(R.id.btnTues);
+        btnWed = view.findViewById(R.id.btnWed);
+        btnThurs = view.findViewById(R.id.btnThurs);
+        btnFri = view.findViewById(R.id.btnFri);
+        btnSat = view.findViewById(R.id.btnSat);
+        btnSun = view.findViewById(R.id.btnSun);
+        btnSun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sunSelect)
+                    sunSelect = false;
+                else
+                    sunSelect = true;
+
+            }
+        });
+        btnMon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (monSelect)
+                    monSelect = false;
+                else
+                    monSelect = true;
+            }
+        });
+
+        btnTues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (tuesSelect)
+                    tuesSelect = false;
+                else
+                    tuesSelect = true;
+            }
+        });
+
+        btnWed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (wedSelect)
+                    wedSelect = false;
+                else
+                    wedSelect = true;
+            }
+        });
+
+        btnThurs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (thursSelect)
+                    thursSelect = false;
+                else
+                    thursSelect = true;
+            }
+        });
+
+        btnFri.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (friSelect)
+                    friSelect = false;
+                else
+                    friSelect = true;
+            }
+        });
+
+        btnSat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (satSelect)
+                    satSelect = false;
+                else
+                    satSelect = true;
+            }
+        });
         AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(RecipeInfoActivity.this);
         dialogBuilder.setView(view);
         dialogBuilder.setCancelable(false)
                 .setPositiveButton("Add to Weekly Recipe", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
+                        days = new ArrayList<>();
+                        if (sunSelect)
+                            days.add("1");
+                        if (monSelect)
+                            days.add("2");
+                        if (tuesSelect)
+                            days.add("3");
+                        if (wedSelect)
+                            days.add("4");
+                        if (thursSelect)
+                            days.add("5");
+                        if (friSelect)
+                            days.add("6");
+                        if (satSelect)
+                            days.add("7");
+                        Log.d("Tester", days.get(0));
+                        for (String d : days)
+                        {
+                            Random random = new Random();
+                            int n = random.nextInt(900000000) + 999999;
+                            Logs addLog = new Logs(d, title, "dom@gmail.com");
+                            database.getReference().child("ZLogs").child("" + n).setValue(addLog);
+
+                        }
                     }
                 })
                 .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
