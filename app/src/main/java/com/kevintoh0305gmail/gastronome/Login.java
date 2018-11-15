8f2fb763104835d975c38498f061f2ce097c688a
Dominic Ng Jun Yao <s10177858@connect.np.edu.sg>
2018-11-15 11:05:51 +0800

    Implement weight calculations

--- app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/Login.java
@@ -37,8 +37,8 @@ public class Login extends AppCompatActivity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent in = new Intent(Login.this, WeekyLog.class);
-                startActivity(in);
+                //Intent in = new Intent(Login.this, WeekyLog.class);
+                //startActivity(in);
                 String email = txtEmail.getText().toString().trim();
                 String password = txtPassword.getText().toString().trim();
 
