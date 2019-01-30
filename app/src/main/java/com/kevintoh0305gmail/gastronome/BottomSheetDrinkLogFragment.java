d50dbf064d4e139ffe34ff5473ea6cfbfaf3f2dd
Kevin Toh <kevintoh0305@gmail.com>
2019-01-30 18:28:42 +0800

    commit

--- app/src/main/java/com/kevintoh0305gmail/gastronome/BottomSheetDrinkLogFragment.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/BottomSheetDrinkLogFragment.java
@@ -1,12 +1,42 @@
 package com.kevintoh0305gmail.gastronome;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
 
 public class BottomSheetDrinkLogFragment extends BottomSheetDialogFragment {
+
+    EditText txtDrinkName, txtCalories;
+    TextView tvDate;
+    ImageButton btnAddImage;
+    Calendar calendar;
+    Date currentDay;
+
+    final int REQUEST_CODE_GALLERY = 999;
 
     public BottomSheetDrinkLogFragment() {
         // Required empty public constructor
@@ -23,5 +53,70 @@ public class BottomSheetDrinkLogFragment extends BottomSheetDialogFragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.bottom_sheet_drinklog, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        txtDrinkName = view.findViewById(R.id.etLogDrinkName);
+        txtCalories = view.findViewById(R.id.etLogDrinkCalories);
+        tvDate = view.findViewById(R.id.tvBottomSheetDrinkDate);
+        btnAddImage = view.findViewById(R.id.btnAddImage2);
+        calendar = Calendar.getInstance();
+        currentDay = calendar.getTime();
+        DateFormat dayFormat = new SimpleDateFormat("EEEE");
+        DateFormat dateFormat = new SimpleDateFormat("dd MMM");
+        tvDate.setText(dayFormat.format(currentDay)+", "+dateFormat.format(currentDay));
+
+
+        btnAddImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ActivityCompat.requestPermissions(getActivity(),
+                        new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                        REQUEST_CODE_GALLERY
+                );
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_CODE_GALLERY){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, REQUEST_CODE_GALLERY);
+            }
+            else{
+                Toast.makeText(getContext(), "The app does not have permissions to access your gallery.",Toast.LENGTH_SHORT).show();
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            try {
+                Uri uri = data.getData();
+                InputStream inputStream = getContext().getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                btnAddImage.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), "File not found.",Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(getContext(), "You haven't picked an image.",Toast.LENGTH_LONG).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
--- app/src/main/java/com/kevintoh0305gmail/gastronome/BottomSheetFoodLogFragment.java
+++ app/src/main/java/com/kevintoh0305gmail/gastronome/BottomSheetFoodLogFragment.java
@@ -1,12 +1,38 @@
 package com.kevintoh0305gmail.gastronome;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.app.Activity.RESULT_OK;
 
 public class BottomSheetFoodLogFragment extends BottomSheetDialogFragment {
+
+
+    EditText txtFoodName, txtCalories;
+    TextView tvDate;
+    Calendar calendar;
+    Date currentDay;
+    ImageButton btnAddImage2;
 
     public BottomSheetFoodLogFragment() {
         // Required empty public constructor
@@ -25,5 +51,36 @@ public class BottomSheetFoodLogFragment extends BottomSheetDialogFragment {
         return inflater.inflate(R.layout.bottom_sheet_foodlog, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        txtFoodName = view.findViewById(R.id.etLogFoodName);
+        txtCalories = view.findViewById(R.id.etLogFoodCalories);
+        tvDate = view.findViewById(R.id.tvBottomSheetDate);
+        btnAddImage2 = view.findViewById(R.id.btnAddImage2);
+        calendar = Calendar.getInstance();
+        currentDay = calendar.getTime();
+        DateFormat dayFormat = new SimpleDateFormat("EEEE");
+        DateFormat dateFormat = new SimpleDateFormat("dd MMM");
+        tvDate.setText(dayFormat.format(currentDay)+", "+dateFormat.format(currentDay));
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            try {
+                Uri uri = data.getData();
+                InputStream inputStream = getContext().getContentResolver().openInputStream(uri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                btnAddImage2.setImageBitmap(bitmap);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(getContext(), "File not found.",Toast.LENGTH_LONG).show();
+            }
+        }
+        else {
+            Toast.makeText(getContext(), "You haven't picked an image.",Toast.LENGTH_LONG).show();
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 }
